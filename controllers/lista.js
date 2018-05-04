9950cbba79f495c7f63f6485eec32e856d3db542
Daniel Perez <djdany.djesus@gmail.com>
2018-05-05 01:41:23 +0200

    v1.0
    v1.0 released

--- controllers/lista.js
+++ controllers/lista.js
@@ -2,116 +2,107 @@ const Lista = require('../models/lista');
 const mongoose = require('mongoose');
 
 function getList(req, res) {
-  var pass = req.body.pass;
+    var pass = req.body.pass;
 
-  Lista.findOne({ pass: pass }, { lista: 1, items: 1, _id: 0 })
-    .sort('+items.nombre')
-    .exec((err, list) => {
-      if (err) {
-        res.status(500).send({ accion: 'get list', error: 'Server Error 500' });
-      } else if (!list) {
-        res.status(404).send({ accion: 'get list', error: 'Lista 404' });
-      } else {
-        res.status(200).send({ accion: 'get list', data: list });
-      }
-    });
+    Lista.findOne({ pass: pass }, { lista: 1, items: 1, _id: 0 })
+        .sort('+items.nombre')
+        .exec((err, list) => {
+            if (err) {
+                res.status(500).send({ accion: 'get list', error: 'Server Error 500' });
+            } else if (!list) {
+                res.status(404).send({ accion: 'get list', error: 'Lista 404' });
+            } else {
+                res.status(200).send({ accion: 'get list', data: list });
+            }
+        });
 }
 
 function newList(req, res) {
-  //db.listas.createIndex( { "pass": 1 }, { unique: true } )
-  var lista = req.body.lista;
-  var pass = req.body.pass;
+    //db.listas.createIndex( { "pass": 1 }, { unique: true } )
+    var lista = req.body.lista;
+    var pass = req.body.pass;
 
-  var list = new Lista();
-  list.lista = lista;
-  list.pass = pass;
+    var list = new Lista();
+    list.lista = lista;
+    list.pass = pass;
 
-  list.save((err, listCreated) => {
-    if (err) {
-      res
-        .status(500)
-        .send({ accion: 'new list', error: 'Contraseña ya existe' });
-    } else {
-      res.status(200).send({ accion: 'new list', data: listCreated });
-    }
-  });
+    list.save((err, listCreated) => {
+        if (err) {
+            res
+                .status(500)
+                .send({ accion: 'new list', error: 'Lista con esa contraseña ya existe' });
+        } else {
+            res.status(200).send({ accion: 'new list', data: listCreated });
+        }
+    });
 }
 
 function delList(req, res) {
-  var pass = req.body.pass;
+    var pass = req.body.pass;
 
-  Lista.findOneAndRemove({ pass: pass }, (err, listDeleted) => {
-    if (err) {
-      res
-        .status(500)
-        .send({ accion: 'delete list', error: 'Server Error 500' });
-    } else {
-      res.status(200).send({ accion: 'delete list', data: listDeleted });
-    }
-  });
+    Lista.findOneAndRemove({ pass: pass }, (err, listDeleted) => {
+        if (err) {
+            res
+                .status(500)
+                .send({ accion: 'delete list', error: 'Server Error 500' });
+        } else {
+            res.status(200).send({ accion: 'delete list', data: listDeleted });
+        }
+    });
 }
 
 function addItem(req, res) {
-  var params = req.body;
+    var params = req.body;
 
-  Lista.update(
-    { pass: params.pass },
-    {
-      $push: {
-        items: {
-          _id: new mongoose.Types.ObjectId(),
-          tachado: params.tachado,
-          nombre: params.nombre,
-          precio: params.precio
+    Lista.update({ pass: params.pass }, {
+        $push: {
+            items: {
+                _id: new mongoose.Types.ObjectId(),
+                tachado: params.tachado,
+                nombre: params.nombre,
+                precio: params.precio
+            }
         }
-      }
-    }
-  ).exec((err, newItem) => {
-    if (err) {
-      res.status(500).send({ accion: 'add item', error: err });
-    } else {
-      res.status(200).send({ accion: 'add item', data: newItem });
-    }
-  });
+    }).exec((err, newItem) => {
+        if (err) {
+            res.status(500).send({ accion: 'add item', error: err });
+        } else {
+            res.status(200).send({ accion: 'add item', data: newItem });
+        }
+    });
 }
 
 function editList(req, res) {
-  var params = req.body;
+    var params = req.body;
 
-  Lista.findOneAndUpdate(
-    { pass: params.pass },
-    { $set: { lista: params.lista } },
-    { new: true },
-    (err, renamedList) => {
-      if (err) {
-        res
-          .status(500)
-          .send({ accion: 'rename list', error: 'Server Error 500' });
-      } else if (renamedList === null) {
-        res.status(404).send({ accion: 'rename list', error: 'Lista 404' });
-      } else {
-        res.status(200).send({ accion: 'rename list', data: renamedList });
-      }
-    }
-  );
+    Lista.findOneAndUpdate({ pass: params.pass }, { $set: { lista: params.lista } }, { new: true },
+        (err, renamedList) => {
+            if (err) {
+                res
+                    .status(500)
+                    .send({ accion: 'rename list', error: 'Server Error 500' });
+            } else if (renamedList === null) {
+                res.status(404).send({ accion: 'rename list', error: 'Lista 404' });
+            } else {
+                res.status(200).send({ accion: 'rename list', data: renamedList });
+            }
+        }
+    );
 }
 
 function delItem(req, res) {
-  var params = req.body;
-  const itemId = new mongoose.Types.ObjectId(params.itemId);
+    var params = req.body;
+    const itemId = new mongoose.Types.ObjectId(params.itemId);
 
-  Lista.update(
-    { pass: params.pass },
-    { $pull: { items: { _id: itemId } } }
-  ).exec((err, delItem) => {
-    if (err) {
-      res
-        .status(500)
-        .send({ accion: 'delete item', error: 'Server Error 500' });
-    } else {
-      res.status(200).send({ accion: 'delete item', data: delItem });
-    }
-  });
+    Lista.update({ pass: params.pass }, { $pull: { items: { _id: itemId } } }).exec((err, delItem) => {
+        if (err) {
+            res
+                .status(500)
+                .send({ accion: 'delete item', error: 'Server Error 500' });
+        } else {
+            res.status(200).send({ accion: 'delete item', data: delItem });
+        }
+    });
 }
 
 module.exports = { getList, newList, delList, editList, addItem, delItem };
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var mongoose = require('mongoose');
 var app = require('./app')
 var port = process.env.PORT || 3678
 
-mongoose.connect('mongodb://localhost:27017/ListaCompra', (err, res) => {
+mongoose.connect('mongodb://listascompartidas:driger123@ds115740.mlab.com:15740/heroku_kg4kr9kd', (err, res) => {
 
     if (err) {
         throw err;
     } else {
         console.log('Conexión a mongodb correcta.')
         app.listen(port, () => {
-            console.log("API ListaCompra funcionando en http://localhost:" + port);
+            console.log("API ListaCompra funcionando");
         });
     }
 });
